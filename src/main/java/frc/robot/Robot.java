8142400052a385dc0adf4b5dc8ed2136a4fa324b
AlphaBots8608 <team8608robotics@gmail.com>
2023-02-14 14:58:50 -0500

    CowboyJoe 2023 Java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,17 +7,31 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 /**
  * This is a demo program showing the use of the DifferentialDrive class. Runs the motors with split
  * arcade steering and an Xbox controller.
  */
 public class Robot extends TimedRobot {
-  private final PWMSparkMax m_leftMotor = new PWMSparkMax(0);
-  private final PWMSparkMax m_rightMotor = new PWMSparkMax(1);
+
+  int frontLeftSparkMaxCanID = 8;
+  int followerLeftSparkMaxCanID = 10;
+  int frontRightSparkMaxCanID = 6;
+  int followerRightSparkMaxCanID = 9;
+
+  int sparkMaxAux1CanID = 15;
+  int sparkMaxAux2CanID = 14;
+
+  private final CANSparkMax m_leftMotor = new CANSparkMax(frontLeftSparkMaxCanID,MotorType.kBrushed);
+  private final CANSparkMax m_leftFollowerMotor = new CANSparkMax(followerLeftSparkMaxCanID,MotorType.kBrushed);
+  private final CANSparkMax m_rightMotor = new CANSparkMax(frontRightSparkMaxCanID,MotorType.kBrushed);
+  private final CANSparkMax m_rightFollowerMotor = new CANSparkMax(followerRightSparkMaxCanID,MotorType.kBrushed);
   private final DifferentialDrive m_robotDrive = new DifferentialDrive(m_leftMotor, m_rightMotor);
   private final XboxController m_driverController = new XboxController(0);
+ 
 
   @Override
   public void robotInit() {
@@ -25,6 +39,9 @@ public class Robot extends TimedRobot {
     // result in both sides moving forward. Depending on how your robot's
     // gearbox is constructed, you might have to invert the left side instead.
     m_rightMotor.setInverted(true);
+
+    m_leftFollowerMotor.follow(m_leftMotor);
+    m_rightFollowerMotor.follow(m_rightMotor);
   }
 
   @Override
@@ -32,6 +49,6 @@ public class Robot extends TimedRobot {
     // Drive with split arcade drive.
     // That means that the Y axis of the left stick moves forward
     // and backward, and the X of the right stick turns left and right.
-    m_robotDrive.arcadeDrive(-m_driverController.getLeftY(), -m_driverController.getRightX());
+    m_robotDrive.arcadeDrive(m_driverController.getLeftY(), m_driverController.getLeftX());
   }
 }
