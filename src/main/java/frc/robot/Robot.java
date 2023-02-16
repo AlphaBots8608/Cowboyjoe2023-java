ad17edb5c0f6176b3653f183318941132138b540
AlphaBots8608 <team8608robotics@gmail.com>
2023-02-16 14:47:16 -0500

    switched to command setup. hope this leads to autonomous added color sensor added subsystems added very basic if color then X setup

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,40 +4,35 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.PowerDistribution;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+//imports for commands
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
+//imports for lasso control/command and encoders
 import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 /**
  * This is a demo program showing the use of the DifferentialDrive class. Runs the motors with split
  * arcade steering and an Xbox controller.
  */
 public class Robot extends TimedRobot {
 
-  private final PowerDistribution m_pdp = new PowerDistribution();
-  
-  int frontLeftSparkMaxCanID = 8;
-  int followerLeftSparkMaxCanID = 10;
-  int frontRightSparkMaxCanID = 6;
-  int followerRightSparkMaxCanID = 9;
+  private Command m_autonomousCommand;
+  private RobotContainer m_robotContainer;
 
-  int sparkMaxAux1CanID = 15;
+
+  JoeColorSensor CSensor= new JoeColorSensor();
+  JoePowerDistributionPanel PDP= new JoePowerDistributionPanel();
+  
+  //private final XboxController m_driverController = new XboxController(0);
+
+  
   int lassoMotorCanID = 16;
-  int sparkMaxAux3CanID = 7;
-
-  private final CANSparkMax m_leftMotor = new CANSparkMax(frontLeftSparkMaxCanID,MotorType.kBrushed);
-  private final CANSparkMax m_leftFollowerMotor = new CANSparkMax(followerLeftSparkMaxCanID,MotorType.kBrushed);
-  private final CANSparkMax m_rightMotor = new CANSparkMax(frontRightSparkMaxCanID,MotorType.kBrushed);
-  private final CANSparkMax m_rightFollowerMotor = new CANSparkMax(followerRightSparkMaxCanID,MotorType.kBrushed);
-  private final DifferentialDrive m_robotDrive = new DifferentialDrive(m_leftMotor, m_rightMotor);
-  private final XboxController m_driverController = new XboxController(0);
-  
   private final CANSparkMax lassoMotor = new CANSparkMax(lassoMotorCanID,MotorType.kBrushless);
   private RelativeEncoder lassoMotor_encoder; 
 
@@ -45,16 +40,18 @@ public class Robot extends TimedRobot {
   //private final CANSparkMax armExtensionMotor = new CANSparkMax(sparkMaxAux1CanID,MotorType.kBrushless);
   
   
+
+
+  
+  
+
   @Override
   public void robotInit() {
-    SmartDashboard.putData("PDP", m_pdp);
-    // We need to invert one side of the drivetrain so that positive voltages
-    // result in both sides moving forward. Depending on how your robot's
-    // gearbox is constructed, you might have to invert the left side instead.
-    m_rightMotor.setInverted(true);
-
-    m_leftFollowerMotor.follow(m_leftMotor);
-    m_rightFollowerMotor.follow(m_rightMotor);
+    // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
+    // autonomous chooser on the dashboard.
+    m_robotContainer = new RobotContainer();
+    
+ 
 
         /**
     * In order to read encoder values an encoder object is created using the 
@@ -64,20 +61,83 @@ public class Robot extends TimedRobot {
   }
 
   @Override
-  public void teleopPeriodic() {
-    // Drive with split arcade drive.
-    // That means that the Y axis of the left stick moves forward
-    // and backward, and the X of the right stick turns left and right.
-    m_robotDrive.arcadeDrive(m_driverController.getLeftY(), m_driverController.getLeftX());
+  public void robotPeriodic() {
+    getEncoderData();
+    PDP.GetPdpData();
+    CSensor.GetColorSensorData();
 
-    ////////////////////////////////////////////
-    lassoMotor.set(m_driverController.getRawAxis(3));
-    ///////////////////////
+    // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
+    // commands, running already-scheduled commands, removing finished or interrupted commands,
+    // and running subsystem periodic() methods.  This must be called from the robot's periodic
+    // block in order for anything in the Command-based framework to work.
+    CommandScheduler.getInstance().run();
+
   }
 
-  @Override
-  public void robotPeriodic() {
+    /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
+    @Override
+    public void autonomousInit() {
+      m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+  
+      // schedule the autonomous command (example)
+      if (m_autonomousCommand != null) {
+        m_autonomousCommand.schedule();
+      }
+    }
+  
+    /** This function is called periodically during autonomous. */
+    @Override
+    public void autonomousPeriodic() {}
 
+
+
+    @Override
+    public void teleopInit() {
+      // This makes sure that the autonomous stops running when
+      // teleop starts running. If you want the autonomous to
+      // continue until interrupted by another command, remove
+      // this line or comment it out.
+      if (m_autonomousCommand != null) {
+        m_autonomousCommand.cancel();
+      }
+    }
+    @Override
+    public void teleopPeriodic() {
+
+  
+      ////////////////////////////////////////////
+      double lassospeed = 0;
+      if (CSensor.lastdetectedColor == "Cube")
+      {
+        lassospeed = m_robotContainer.joystick1.getRawAxis(3)/2;
+      }
+      else if (CSensor.lastdetectedColor == "Cone")
+      {
+        lassospeed = m_robotContainer.joystick1.getRawAxis(3);
+      }
+      lassoMotor.set(lassospeed);
+      ///////////////////////
+    }
+
+  /** This function is called once each time the robot enters Disabled mode. */
+  @Override
+  public void disabledInit() {}
+
+  @Override
+  public void disabledPeriodic() {}
+
+  @Override
+  public void testInit() {
+    // Cancels all running commands at the start of test mode.
+    CommandScheduler.getInstance().cancelAll();
+  }
+
+  /** This function is called periodically during test mode. */
+  @Override
+  public void testPeriodic() {}
+
+  public void getEncoderData()
+  {
     /**
      * Encoder position is read from a RelativeEncoder object by calling the
      * GetPosition() method.
@@ -94,35 +154,6 @@ public class Robot extends TimedRobot {
      */
     SmartDashboard.putNumber("Encoder Velocity", lassoMotor_encoder.getVelocity());
 
-    /////////////////////////////////
-
-    // Get the current going through channel 7, in Amperes.
-    // The PDP returns the current in increments of 0.125A.
-    // At low currents the current readings tend to be less accurate.
-    double current7 = m_pdp.getCurrent(7);
-    SmartDashboard.putNumber("Current Channel 7", current7);
-
-    // Get the voltage going into the PDP, in Volts.
-    // The PDP returns the voltage in increments of 0.05 Volts.
-    double voltage = m_pdp.getVoltage();
-    SmartDashboard.putNumber("Voltage", voltage);
-
-    // Retrieves the temperature of the PDP, in degrees Celsius.
-    double temperatureCelsius = m_pdp.getTemperature();
-    SmartDashboard.putNumber("Temperature", temperatureCelsius);
-
-    // Get the total current of all channels.
-    //double totalCurrent = m_pdp.getTotalCurrent();
-    //SmartDashboard.putNumber("Total Current", totalCurrent);
-
-    // Get the total power of all channels.
-    // Power is the bus voltage multiplied by the current with the units Watts.
-    //double totalPower = m_pdp.getTotalPower();
-    //SmartDashboard.putNumber("Total Power", totalPower);
-
-    // Get the total energy of all channels.
-    // Energy is the power summed over time with units Joules.
-    //double totalEnergy = m_pdp.getTotalEnergy();
-    //SmartDashboard.putNumber("Total Energy", totalEnergy);
   }
+  
 }
---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,57 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+
+
+public class RobotContainer {
+    //CONFIG
+    int DRIVEJoystickPort = 0;
+    int DRIVEforwardReverseAxis = 0;
+    int DRIVELeftRightAxis = 1;
+
+
+    private final DriveSubsystem driveSubsystem = new DriveSubsystem();
+    // private final ElevatorSubsystem elevatorSubsystem = new ElevatorSubsystem();
+    // private final IntakeSubsystem intakeSubsystem = new IntakeSubsystem();
+    
+    public final Joystick joystick1 = new Joystick(DRIVEJoystickPort);
+
+    public RobotContainer() {
+        configureButtonBindings();
+
+        driveSubsystem.setDefaultCommand(new ArcadeDriveCmd(driveSubsystem, //
+                () -> joystick1.getRawAxis(DRIVEforwardReverseAxis),
+                () -> joystick1.getRawAxis(DRIVELeftRightAxis))//
+        );
+        // elevatorSubsystem.setDefaultCommand(new ElevatorJoystickCmd(elevatorSubsystem, 0));
+        // intakeSubsystem.setDefaultCommand(new IntakeSetCmd(intakeSubsystem, true));
+    }
+
+    private void configureButtonBindings() {
+        // new JoystickButton(joystick1, OIConstants.kElevatorPIDRaiseButtonIdx)
+        //         .whileActiveOnce(new ElevatorPIDCmd(elevatorSubsystem, ElevatorConstants.kRaisedPosition));
+        // new JoystickButton(joystick1, OIConstants.kElevatorPIDLowerButtonIdx)
+        //         .whileActiveOnce(new ElevatorPIDCmd(elevatorSubsystem, ElevatorConstants.kLoweredPosition));
+        // new JoystickButton(joystick1, OIConstants.kElevatorJoystickRaiseButtonIdx)
+        //         .whileActiveOnce(new ElevatorJoystickCmd(elevatorSubsystem, ElevatorConstants.kJoystickMaxSpeed));
+        // new JoystickButton(joystick1, OIConstants.kElevatorJoystickLowerButtonIdx)
+        //         .whileActiveOnce(new ElevatorJoystickCmd(elevatorSubsystem, -ElevatorConstants.kJoystickMaxSpeed));
+        // new JoystickButton(joystick1, OIConstants.kIntakeCloseButtonIdx)
+        //         .whileActiveOnce(new IntakeSetCmd(intakeSubsystem, false));
+    }
+
+    public Command getAutonomousCommand() {
+        return new SequentialCommandGroup();
+        // return new SequentialCommandGroup( //
+        //         new DriveForwardCmd(driveSubsystem, DriveConstants.kAutoDriveForwardDistance), //
+        //         new ParallelCommandGroup( //
+        //                 new IntakeSetCmd(intakeSubsystem, false), //
+        //                 new ElevatorPIDCmd(elevatorSubsystem, ElevatorConstants.kRaisedPosition) //
+        //         )//
+        // );
+    }
+}
